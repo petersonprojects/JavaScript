fe47d43d01b609f11b532a9a7c515361827c9e47
Micah Peterson <micahpeterson@live.com>
2020-06-23 16:53:58 -0500

    only pos numbers

---
+++ exercises2/positives.js
@@ -0,0 +1,18 @@
+
+function posNums(numArray)
+{
+    var tempList = [];
+    for(var x = 0; x<numArray.length;x++)
+    {
+        if(numArray[x] > 0)
+        {
+            tempList.push(numArray[x]);
+        }
+    }
+
+    return tempList;
+}
+
+var example = [-5,-3,-2,0,1,2,3,5];
+
+console.log(posNums(example));
