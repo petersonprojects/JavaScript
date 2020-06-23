eb6464d451ddeb4dbd643177fc1c8f6300286f19
Micah Peterson <micahpeterson@live.com>
2020-06-23 16:39:44 -0500

    long vowels

---
+++ exercises2/longvowels.js
@@ -0,0 +1,24 @@
+function makeLong(string)
+{
+    var splitUp = string.split('');
+
+    for(var x = 0; x<splitUp.length;x++)
+    {
+        if(splitUp[0] != 'e' || splitUp[splitUp.length-1] != 'e')
+        {
+            if(splitUp[x] == 'e' || splitUp[x] == 'E')
+            {
+                splitUp[x] = 'eeee';
+            }
+            if(splitUp[x] == 'o' || splitUp[x] == 'O')
+            {
+                splitUp[x] = 'oooo';
+            }
+        }
+
+    }
+    var newArray = splitUp.join('');
+    return newArray;
+}
+
+console.log(makeLong("Cheech and Chong"));
