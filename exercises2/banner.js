1c064ed1a95f5d8c6ee9f9fbe89264dcdb020b5d
Micah Peterson <micahpeterson@live.com>
2020-06-23 15:53:28 -0500

    banner

---
+++ exercises2/banner.js
@@ -0,0 +1,21 @@
+
+
+function makeBanner(string)
+{
+    for(var x=0;x<string.length;x++)
+    {
+        process.stdout.write("*");
+    }
+    process.stdout.write("****");
+    console.log("");
+    process.stdout.write("* " +string +" *");
+    console.log("");
+    for(var x=0;x<string.length;x++)
+    {
+        process.stdout.write("*");
+    }
+    process.stdout.write("****");
+    console.log("");
+}
+
+makeBanner("Easy banner");
