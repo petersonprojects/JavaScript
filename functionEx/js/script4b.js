43b7a31cacd64cf5f17f87e609f5b6cf52b0a46d
Micah Peterson <micahpeterson@live.com>
2020-06-25 21:01:01 -0500

    good job

---
+++ functionEx/js/script4b.js
@@ -0,0 +1,10 @@
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 } ];
+
+    cities.forEach(function(cityObj){
+        console.log(cityObj.name);
+    })
+
