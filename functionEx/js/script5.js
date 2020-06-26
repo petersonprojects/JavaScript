0147d653e1b9fd4dbfa3052b7a42411c5c9abb0f
Micah Peterson <micahpeterson@live.com>
2020-06-25 21:04:50 -0500

    good job done

--- functionEx/js/script5.js
+++ functionEx/js/script5.js
@@ -1,1 +1,10 @@
 // Goob Job!
+var people = [ 'Dom', 'Lyn', 'Kirk', 'Autumn', 'Trista', 
+                'Jesslyn', 'Kevin', 'John', 'Eli', 'Juan',
+                 'Robert', 'Keyur', 'Jason', 'Che', 'Ben' ];
+
+var goodjob = people.map(function(newString){
+    return `Good job, ${newString}!`
+})
+
+console.log(goodjob);
