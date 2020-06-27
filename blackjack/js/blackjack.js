c8e581979bad907f75d45fc2c4c262ca0ff6eb87
Micah Peterson <micahpeterson@live.com>
2020-06-26 22:04:36 -0500

    added hit feature

--- blackjack/js/blackjack.js
+++ blackjack/js/blackjack.js
@@ -1,16 +1,91 @@
-//build a deck of cards
+// //build a deck of cards
+// var deck = [{suit:'',points:0}];
+// var suit = 'hearts'
+// var points = 0;
 
+// for(var x=0;x<53;x++)
+// {
+//     if(points==13||points==26||points==39||points==52)
+//         {
+//             points=0;
 
-// var deck = [
-//     {name: 'ace', suit: 'heart', values: [1,11], img: },
-//     {name: 'ace', suit: 'spade', values: [1,11], img: },
-// ];
+//             if(deck[x][suit]=='hearts'){
+//                 deck[x].suit='spades'
+//             }
+//             else if(deck[x].suit=='spades'){
+//                 deck[x].suit='clubs'
+//             }
+//             else if(deck[x].suit=='clubs'){
+//                 deck[x].suit='diamonds'
+//             }
+//             else if(deck[x].suit=='diamonds'){
+//                 deck[x].suit='hearts'
+//             }
+//         }
 
-// var playersHand = [];
-// var dealersHand = [];
+//     points++;
+//     console.log(points);
 
-//when the hands are dealt, pop off 4 cards from the array
+//     deck[points]= points;
+//     deck[suit]=suit;
+// }
 
+// console.log(deck);
+
+var deck = [
+    {value:1,suit:'hearts'},
+    {value:2,suit:'hearts'},
+    {value:3,suit:'hearts'},
+    {value:4,suit:'hearts'},
+    {value:5,suit:'hearts'},
+    {value:6,suit:'hearts'},
+    {value:7,suit:'hearts'},
+    {value:8,suit:'hearts'},
+    {value:9,suit:'hearts'},
+    {value:10,suit:'hearts'},
+    {value:11,suit:'hearts'},
+    {value:12,suit:'hearts'},
+    {value:13,suit:'hearts'},
+    {value:1,suit:'diamonds'},
+    {value:2,suit:'diamonds'},
+    {value:3,suit:'diamonds'},
+    {value:4,suit:'diamonds'},
+    {value:5,suit:'diamonds'},
+    {value:6,suit:'diamonds'},
+    {value:7,suit:'diamonds'},
+    {value:8,suit:'diamonds'},
+    {value:9,suit:'diamonds'},
+    {value:10,suit:'diamonds'},
+    {value:11,suit:'diamonds'},
+    {value:12,suit:'diamonds'},
+    {value:13,suit:'diamonds'},
+    {value:1,suit:'clubs'},
+    {value:2,suit:'clubs'},
+    {value:3,suit:'clubs'},
+    {value:4,suit:'clubs'},
+    {value:5,suit:'clubs'},
+    {value:6,suit:'clubs'},
+    {value:7,suit:'clubs'},
+    {value:8,suit:'clubs'},
+    {value:9,suit:'clubs'},
+    {value:10,suit:'clubs'},
+    {value:11,suit:'clubs'},
+    {value:12,suit:'clubs'},
+    {value:13,suit:'clubs'},
+    {value:1,suit:'spades'},
+    {value:2,suit:'spades'},
+    {value:3,suit:'spades'},
+    {value:4,suit:'spades'},
+    {value:5,suit:'spades'},
+    {value:6,suit:'spades'},
+    {value:7,suit:'spades'},
+    {value:8,suit:'spades'},
+    {value:9,suit:'spades'},
+    {value:10,suit:'spades'},
+    {value:11,suit:'spades'},
+    {value:12,suit:'spades'},
+    {value:13,suit:'spades'},
+];
 
 function shuffleDeck(deck) 
 {
@@ -24,41 +99,57 @@ function shuffleDeck(deck)
     return deck;
 }
 
+var newDeck = shuffleDeck(deck);
+
+var playersHand = [];
+var dealersHand = [];
+
+//when the hands are dealt, pop off 4 cards from the array
+
+
 //"listens" for click, and then executes
 // the code inside the code block.  
 //Note that the listenter takes as arguments, the event,
 // and anfn.
 var dh = document.getElementById("dealer-hand");
-
-var img1 = document.createElement('img');
-img1.src="JPEG/purple_back.jpg"
-dh.appendChild(img1);
-
-var img2 = document.createElement('img');
-img2.src="JPEG/10S.jpg"
-img2.className="ml-3"
-dh.appendChild(img2);
-
-//player hand
 var ph = document.getElementById("player-hand");
 
-var img3 = document.createElement('img');
-img3.src="JPEG/AS.jpg"
-ph.appendChild(img3);
 
-var img4 = document.createElement('img');
-img4.src="JPEG/KH.jpg"
-img4.className="ml-3"
-ph.appendChild(img4);
 
 document.getElementById("deal-button").addEventListener("click", function(){
     //write dealer logic here
     //retrieve dealer hand
+    var img1 = document.createElement('img');
+    img1.src="JPEG/purple_back.jpg"
+    dh.appendChild(img1);
+
+    var img2 = document.createElement('img');
+    img2.src="JPEG/10S.jpg"
+    img2.className="ml-3"
+    dh.appendChild(img2);
+
+    //player hand
+
+
+    var img3 = document.createElement('img');
+    img3.src="JPEG/AS.jpg"
+    ph.appendChild(img3);
+
+    var img4 = document.createElement('img');
+    img4.src="JPEG/KH.jpg"
+    img4.className="ml-3"
+    ph.appendChild(img4);
 
 });
+
 document.getElementById("hit-button").addEventListener("click", function(){
     //write "hit" logic here
+    var hit = document.createElement('img');
+    hit.src="JPEG/5S.jpg"
+    hit.className="ml-3"
+    ph.appendChild(hit);
 });
+
 document.getElementById("stand-button").addEventListener("click", function(){
     //write "Stand" logic here
 });
