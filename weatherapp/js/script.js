ee957d2c49f59ea8309ee4df3c161fdc56e8ee7a
Micah Peterson <micahpeterson@live.com>
2020-07-01 17:43:46 -0500

    styling

--- weatherapp/js/script.js
+++ weatherapp/js/script.js
@@ -10,7 +10,10 @@ $(()=>{
             let kelvin = data.main.temp;
             let degC = kelvin - 273.15;
             let degF = Math.floor(degC * 1.8 + 32);
-            $('ul').append(`<li>Current temp ${city} (F): ${degF}</li>`);
+            $('ul').append(`<li class="l-item">${city}(F) ${degF}°</li>`);
+        })
+        .fail(()=>{
+            alert("That's not a valid city name");
         })
     })
 
