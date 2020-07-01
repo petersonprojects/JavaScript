66e9ecb5e022fa6aaea92004f8d44eced88900ff
Micah Peterson <micahpeterson@live.com>
2020-07-01 18:37:21 -0500

    make it so you can also search with enter and input clears

--- weatherapp/js/script.js
+++ weatherapp/js/script.js
@@ -2,11 +2,12 @@
 //jQuery
 $(()=>{
 
-    $('#enter').click((event)=>{
+    function getinfo()
+    {
         let city = $('input').val()
-
         $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c2632fd651b3b04bf62b614a0189cfd4`)
         .done((data)=>{
+
             let kelvin = data.main.temp;
             let degC = kelvin - 273.15;
             let degCInt = Math.floor(kelvin - 273.15);
@@ -29,6 +30,19 @@ $(()=>{
         .fail(()=>{
             alert("That's not a valid city name");
         })
+    }
+
+    $('#enter').click((event)=>{
+        getinfo()
+        $('input').val('')
+    })
+
+    $('input').keydown((sam)=>{
+        if(sam.which == 13)
+        {
+            getinfo()
+            $('input').val('')
+        }
     })
 
     $('#clear').click((event)=>{
