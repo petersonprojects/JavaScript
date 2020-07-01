84dae2282553cb1e94beedb833e3c5dff6b38694
Micah Peterson <micahpeterson@live.com>
2020-07-01 16:35:47 -0500

    weatherapp repo

--- weatherapp/js/script.js
+++ weatherapp/js/script.js
@@ -1,31 +1,21 @@
 
-            //jQuery
-            $(()=>{
+//jQuery
+$(()=>{
 
-                //this is a promise
-                $.get(`http://api.openweathermap.org/data/2.5/weather?q=houston&appid=c2632fd651b3b04bf62b614a0189cfd4`)
-                //jquery doesnt require json conversion
-                //chain as many as you done as u want in order of exec
-                .done((data)=>{
+    $('#enter').click((event)=>{
+        let city = $('input').val()
 
-                    $('button').click((event)=>{
-                        let city = $('input').val()
+        $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c2632fd651b3b04bf62b614a0189cfd4`)
+        .done((data)=>{
+            let kelvin = data.main.temp;
+            let degC = kelvin - 273.15;
+            let degF = Math.floor(degC * 1.8 + 32);
+            $('ul').append(`<li>Current temp ${city} (F): ${degF}</li>`);
+        })
+    })
 
-                        $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c2632fd651b3b04bf62b614a0189cfd4`)
-                        .done((data)=>{
-                            let kelvin = data.main.temp;
-                            let degC = kelvin - 273.15;
-                            let degCInt = Math.floor(degC);
-                            let degF = degC * 1.8 + 32;
-                            let degFInt = Math.floor(degF);
-                            $('ul').append(`<li>Current temp ${city} (F): ${degFInt}</li>`);
-                        })
-                    })
+    $('#clear').click((event)=>{
+        $('input').val('')
+    })
 
-                })
-                //catches errors
-                .then((error)=>{
-                    //console.log(error);
-                })
-
-            })
+})
