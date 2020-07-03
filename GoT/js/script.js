c902721d077b871b4fd96b4e26ba859e4b0490f5
Micah Peterson <micahpeterson@live.com>
2020-07-03 16:02:40 -0500

    gucci mayng

--- GoT/js/script.js
+++ GoT/js/script.js
@@ -5,77 +5,109 @@ $(()=>{
     {
 
         let characterArray = [];
+        let houseURLs = [];
 
         let p1 = new Promise((resolve, reject)=>{
-            for(let i = 1;i < 50; i++)
+            
+            for(let i = 1;i < 25; i++)
             {
-                characterArray.push($.get(`https://anapioficeandfire.com/api/characters/${i}`))
-                var houses = characterArray.map((characters)=>{
-                    if(characters.allegiances.length > 0)
-                    {
-                        let houseURL = 
-                    }
-                    else if(character.allegiances.length == 0)
-                    {
+                $.get(`https://anapioficeandfire.com/api/characters/${i}`)
+                .done((api_chars)=>{
 
+                    if(api_chars.name.length > 0)
+                    {
+                        characterArray.push(api_chars.name)
                     }
+                    else{
+                        characterArray.push(api_chars.aliases[0]);
+                    }
+                    if(api_chars.allegiances.length > 0)
+                    {
+                        houseURLs.push(api_chars.allegiances[0])
+                    }
+                    else{
+                        houseURLs.push(' No allegiances')
+                    }
+
+                    console.log(`Char array: ${characterArray}`);
+                    console.log(`House URL array: ${houseURLs}`);
+
+                    let houseNames = houseURLs.map((houses)=>{
+
+                        if(houses != ' No allegiances')
+                        {
+                            $.get(houses)
+                            .done(()=>{
+                                return houses.name;
+                            })
+                        }
+                        else{
+                            return ' No allegiances'
+                        }
+
+                        console.log(`houses: ${houses}`);
+
+                    })
+                    console.log(houseNames);
+                })
+                .then(()=>{
+                    resolve("Success creating arrays.");
                 })
             }
-            resolve();
-        });
-        let p2 = new Promise((resolve, reject)=>{
-            for(let i = 1;i < 50; i++)
+
+        })
+        .then((characterArray, houseNames)=>{
+
+            for(let i = 0;i < 24;i++)
             {
-                houses.push($.get(`https://anapioficeandfire.com/api/houses/`))
-                resolve();
+                $('ul').append(`<li class="cold col-4 mt-3">${characterArray[i]}: <button class="btn btn-dark">${houseNames[i]}</li>`);
             }
-        });
+        })
     
-        Promise.all([characterArray, housesArray])
-        .then((data)=>{
-            console.log(data);
-            resolve(data);
-        })
-        .then(()=>{
-            //get the house api
-        })
+        // Promise.all([p1, p2])
+        // .then((data)=>{
+        //     console.log(data);
+        // })
+        // .then(()=>{
+        //     //get the house api
+        // })
 
-        for(let i = 1;i < 50; i++)
-        {
-            $.get(`https://anapioficeandfire.com/api/characters/${i}`)
-            .done((character)=>{
-                $.get(character.allegiances[0])
-                .done((houses)=>{
+        // for(let i = 1;i < 50; i++)
+        // {
+        //     $.get(`https://anapioficeandfire.com/api/characters/${i}`)
+        //     .done((character)=>{
+        //         $.get(character.allegiances[0])
+        //         .done((houses)=>{
 
-                    if(character.allegiances.length > 0)
-                    {
-                        if(character.name.length == 0)
-                        {
-                            $('ul').append(`<li class="cold col-4 mt-3">${character.aliases}: <button class="btn btn-dark">${houses.name}</li>`);
-                        }
-                        else{
-                            $('ul').append(`<li class="cold col-4 mt-3">${character.name}: <button class="btn btn-dark">${houses.name}</li>`);
-                        }
-                    }
-                    else if(character.allegiances.length == 0)
-                    {
-                        if(character.name.length == 0)
-                        {
-                            $('ul').append(`<li class="cold col-4 mt-3">${character.aliases}: <button class="btn btn-dark">No Allegiances</li>`);
-                        }
-                        else{
-                            $('ul').append(`<li class="cold col-4 mt-3">${character.name}: <button class="btn btn-dark">No Allegiances</li>`);
-                        }
-                    }
-                })
-                .fail(()=>{
-                    alert("Api house loading failed.");
-                })
-            })
-            .fail(()=>{
-                alert("Api character loading failed.");
-            })
-        } //end for loop
+        //             if(character.allegiances.length > 0)
+        //             {
+        //                 if(character.name.length == 0)
+        //                 {
+        //                     $('ul').append(`<li class="cold col-4 mt-3">${character.aliases}: <button class="btn btn-dark">${houses.name}</li>`);
+        //                 }
+        //                 else{
+        //                     $('ul').append(`<li class="cold col-4 mt-3">${character.name}: <button class="btn btn-dark">${houses.name}</li>`);
+        //                 }
+        //             }
+        //             else if(character.allegiances.length == 0)
+        //             {
+        //                 if(character.name.length == 0)
+        //                 {
+        //                     $('ul').append(`<li class="cold col-4 mt-3">${character.aliases}: <button class="btn btn-dark">No Allegiances</li>`);
+        //                 }
+        //                 else{
+        //                     $('ul').append(`<li class="cold col-4 mt-3">${character.name}: <button class="btn btn-dark">No Allegiances</li>`);
+        //                 }
+        //             }
+        //         })
+        //         .fail(()=>{
+        //             alert("Api house loading failed.");
+        //         })
+        //     })
+        //     .fail(()=>{
+        //         alert("Api character loading failed.");
+        //     })
+        // } //end for loop
     } // end of getinfo function
 
     function getHouseInfo(char_index)
@@ -105,11 +137,11 @@ $(()=>{
 
     getinfo()
 
-    $('ul').on('click','li',(e)=>{
-        var listItem = (e.target.parentNode)
-        var index = $('li').index(listItem);
-        console.log(index);
-        getHouseInfo(index);
-    })
+    // $('ul').on('click','li',(e)=>{
+    //     var listItem = (e.target.parentNode)
+    //     var index = $('li').index(listItem);
+    //     console.log(index);
+    //     getHouseInfo(index);
+    // })
 
 }); //end of JQuery
