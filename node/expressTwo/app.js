4467b665145c7ab8c04f3077fd73f114d0e959b6
Micah Peterson <micahpeterson@live.com>
2020-07-16 11:40:08 -0500

    express request queries

--- node/expressTwo/app.js
+++ node/expressTwo/app.js
@@ -3,14 +3,35 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3002,()=>{
-    console.log('listening on port 3002');
-})
-
 app.get('/',(req,res)=>{
     res.send(`<h1>Second time making this bad boy</h1>`)
 })
 
-app.get('/aboutus',(req,res)=>{
-    res.send(`<h1>about us!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>`)
+
+// contact?name=Micah&lname=Peterson
+app.get('/contact',(req,res)=>{
+    let fname = req.param('name')
+    let lName = req.param('lname')
+    res.send(`My name is ${fname} ${lName}`)
+})
+
+// app.get('/aboutus',(req,res)=>{
+//     res.send(`<h1>about us!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>`)
+// })
+
+// app.get('/cats', (req,res)=>{
+//     res.send('<h1>Cat Page</h1>')
+// })
+
+// app.get('/pups', (req,res)=>{
+//     res.send('<h1>Pup Page</h1>')
+// })
+
+
+// app.get('/yungtest', (req,res) => {
+//     res.send('testing123')
+// })
+
+app.listen(3002,()=>{
+    console.log('listening on port 3002');
 })
