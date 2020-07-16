2b779b9636544bec02e6f7ba0ac05e5c305a10e7
Micah Peterson <micahpeterson@live.com>
2020-07-16 13:18:44 -0500

    expres pup photo

--- node/expressTwo/app.js
+++ node/expressTwo/app.js
@@ -4,16 +4,41 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send(`<h1>Second time making this bad boy</h1>`)
+    res.send(`<h1>Home page.</h1>`)
+})
+
+app.get('/calc/:x,:y',(req,res)=>{
+    let x = parseInt(req.params.x)
+    let y = parseInt(req.params.y)
+    let sum = x+y;
+    res.send(`The sum of ${x} and ${y} is ${sum}`)
+})
+
+app.get('/photo?:img',(req,res)=>{
+    res.send(`<img src="${req.query.images}">`)
 })
 
 
 // contact?name=Micah&lname=Peterson
-app.get('/contact',(req,res)=>{
-    let fname = req.param('name')
-    let lName = req.param('lname')
-    res.send(`My name is ${fname} ${lName}`)
-})
+// app.get('/contact',(req,res)=>{
+    // let fname = req.param('name')
+    // let lName = req.param('lname')
+
+//     let fname = req.query.name;
+//     let lName = req.query.lname;
+
+//     res.send(`My name is ${fname} ${lName}`)
+// })
+
+// app.get('/contact/:fname',(req,res)=>{
+
+//     let name = req.params.fname;
+//     res.send(`My first name is: ${name}`)
+// })
+// app.get('/flights/:from/:to',(req,res)=>{
+
+//     res.send(`Depart from: ${req.params.from} Arriving at: ${req.params.to}`)
+// })
 
 // app.get('/aboutus',(req,res)=>{
 //     res.send(`<h1>about us!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>`)
