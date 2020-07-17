49bc8fe59894007f7ec8b80a21bb20f15d29969e
Micah Peterson <micahpeterson@live.com>
2020-07-17 11:23:48 -0500

    embedding js in html

--- node/classwork/app.js
+++ node/classwork/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs'); //views 
+app.set('views', 'views'); //use this if you named your views folder something else
+
 app.use(express.static('public'));
 
 app.use(require('./routes/index'));
--- node/classwork/routes/index.js
+++ node/classwork/routes/index.js
@@ -3,12 +3,41 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res)=>{
-    res.send(`
 
-    <link rel="stylesheet" href="styles.css">
-    <h1 class="highlight">Front Page</h1>
-    
-    `);
+    let pictures = [
+        "https://images-na.ssl-images-amazon.com/images/I/71qWfXW6tbL._AC_SL1400_.jpg",
+        "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/5-houston-skyline-library-of-congress.jpg",
+        "https://images.fineartamerica.com/images-medium-large-5/houston-skyline-p-a-thompson.jpg"
+    ];
+
+    let names = [
+        "Micah",
+        "Woody",
+        "RJ",
+        "Jeremy",
+        "Chris",
+        "Dan",
+        "Cainan",
+        "Michael",
+        "Daniel",
+        "Veronica"
+    ];
+
+    let cities = [
+        "Atlanta",
+        "Houston",
+        "Seattle",
+        "Miami"
+    ];
+
+    res.render('index', {
+        firstName: "Micah",
+        lastName: "Peterson",
+        pic: pictures,
+        names: names,
+        cities: cities
+    });
+
 });
 
 module.exports = router;
